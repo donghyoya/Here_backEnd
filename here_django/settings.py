f3fd9d328f43b1e900172b3cbf47af4c66f353b6
kdw <dongwooya123@gmail.com>
2023-09-01 14:12:25 +0900

    move file

--- here_django/settings.py
+++ here_django/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'here_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'root',
+        'PASSWORD': '0814',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
